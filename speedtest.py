a641aa8129a2e146209dba3a8f75acb0b9d40455
Victor Salvador Martinez <vmartinez@gmail.com>
2021-05-05 01:48:42 -0500

    while true

--- speedtest.py
+++ speedtest.py
@@ -2,32 +2,33 @@ import re
 import subprocess
 from influxdb import InfluxDBClient
 
-response = subprocess.Popen('/usr/bin/speedtest --accept-license', shell=True, stdout=subprocess.PIPE).stdout.read().decode('utf-8')
+while True:
+    response = subprocess.Popen('/usr/bin/speedtest --accept-license', shell=True, stdout=subprocess.PIPE).stdout.read().decode('utf-8')
 
-ping = re.search('Latency:\s+(.*?)\s', response, re.MULTILINE)
-download = re.search('Download:\s+(.*?)\s', response, re.MULTILINE)
-upload = re.search('Upload:\s+(.*?)\s', response, re.MULTILINE)
-jitter = re.search('\((.*?)\s.+jitter\)\s', response, re.MULTILINE)
+    ping = re.search('Latency:\s+(.*?)\s', response, re.MULTILINE)
+    download = re.search('Download:\s+(.*?)\s', response, re.MULTILINE)
+    upload = re.search('Upload:\s+(.*?)\s', response, re.MULTILINE)
+    jitter = re.search('\((.*?)\s.+jitter\)\s', response, re.MULTILINE)
 
-ping = ping.group(1)
-download = download.group(1)
-upload = upload.group(1)
-jitter = jitter.group(1)
+    ping = ping.group(1)
+    download = download.group(1)
+    upload = upload.group(1)
+    jitter = jitter.group(1)
 
-speed_data = [
-    {
-        "measurement" : "internet_speed",
-        "tags" : {
-            "host": "RaspberryPiMyLifeUp"
-        },
-        "fields" : {
-            "download": float(download),
-            "upload": float(upload),
-            "ping": float(ping),
-            "jitter": float(jitter)
+    speed_data = [
+        {
+            "measurement" : "internet_speed",
+            "tags" : {
+                "host": "RaspberryPiMyLifeUp"
+            },
+            "fields" : {
+                "download": float(download),
+                "upload": float(upload),
+                "ping": float(ping),
+                "jitter": float(jitter)
+            }
         }
-    }
-]
-client = InfluxDBClient('localhost', 8086, 'speedmonitor', 'pimylifeup', 'internetspeed')
+    ]
+    client = InfluxDBClient('localhost', 8086, 'speedmonitor', 'pimylifeup', 'internetspeed')
 
-client.write_points(speed_data)
+    client.write_points(speed_data)
